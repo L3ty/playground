89e978bcadd42e00524d764f14575156d694b515
leticia <gomezleticia650@gmail.com>
2023-09-01 16:18:08 -0300

    pokedex

---
+++ pokedex/main.c
@@ -0,0 +1,15 @@
+//foi executado no prompt de comando windows
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int pk, pj, ph;
+int pkDois, pjDois, phDois;
+
+cin >>pk>> pj>>ph;
+cin >>pkDois>> pjDois>>phDois;
+cout<<pk+pkDois<<" "<<pj+pjDois<<" "<<ph+phDois<<endl;
+   
+}
